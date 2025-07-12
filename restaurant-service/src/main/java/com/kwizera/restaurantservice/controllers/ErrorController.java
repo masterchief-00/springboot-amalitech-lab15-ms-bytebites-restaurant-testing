09fd870ac4aa096313eea129e1b5e2ee72bd5e0e
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2025-07-12 18:14:00 +0200

    chore: added sl4j logger in the error controller and retaurant controller

--- restaurant-service/src/main/java/com/kwizera/restaurantservice/controllers/ErrorController.java
+++ restaurant-service/src/main/java/com/kwizera/restaurantservice/controllers/ErrorController.java
@@ -2,15 +2,22 @@ package com.kwizera.restaurantservice.controllers;
 
 import com.kwizera.restaurantservice.domain.dtos.ApiErrorResponse;
 import com.kwizera.restaurantservice.exceptions.UnauthorizedAccessException;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RequiredArgsConstructor
 @RestControllerAdvice
 public class ErrorController {
+    private static final Logger logger = LoggerFactory.getLogger(ErrorController.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+        logger.error(ex.getMessage());
         ApiErrorResponse errorResponse = ApiErrorResponse.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
                 .message("An unexpected error occurred")
@@ -20,6 +27,7 @@ public class ErrorController {
 
     @ExceptionHandler(UnauthorizedAccessException.class)
     public ResponseEntity<ApiErrorResponse> handleUnauthorizedAccessException(UnauthorizedAccessException ex) {
+        logger.error("Unauthorized access blocked :" + ex.getMessage());
         ApiErrorResponse errorResponse = ApiErrorResponse.builder()
                 .status(HttpStatus.UNAUTHORIZED.value())
                 .message(ex.getMessage())
